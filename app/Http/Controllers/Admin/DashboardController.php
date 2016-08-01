62cddee9fc0a3846159e2c80bb500e99f931b549
xuxujie <xuxujie@langtianhealth.com>
2016-08-02 00:58:25 +0800

    便跟

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -7,6 +7,6 @@ use App\Http\Controllers\Controller;
 class DashboardController extends Controller
 {
     public function index(){
-        return view('admin.dashboard');
+        return view('dashboard.dashboard');
     }
 }
